7e2083e5b0f0853f29b68d8d95c2476a90ca2c85
D4rkSl4yer <113204247+D4rkSl4yer@users.noreply.github.com>
2022-12-02 15:11:12 +0100

    Add files via upload

---
+++ Cseveges/Cseveges/Beszelgetes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cseveges
+{
+    internal class Beszelgetes
+    {
+        public static List<Beszelgetes> Beszelgetesek = new List<Beszelgetes>();
+        public static List<String> Tagok = new List<string>();
+        public DateTime Kezdet { get; set; }
+        public DateTime Veg { get; set; }
+        public string Kezdemenyezo { get; set; }
+        public string Fogado { get; set; }
+        public Beszelgetes(string szöveg) 
+        {
+            string[] dbok = szöveg.Split(';');
+            dbok[0] = dbok[0].Replace('-', ' ');
+            dbok[1] = dbok[1].Replace('-', ' ');
+            Kezdet = DateTime.Parse(dbok[0]);
+            Veg = DateTime.Parse(dbok[1]);
+            Kezdemenyezo= dbok[2];
+            Fogado= dbok[3];
+        }
+        
+    }
+}
---
+++ Cseveges/Cseveges/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Cseveges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            feladat3();
+            feladat4();
+            feladat5();
+            feladat6();
+            Console.ReadKey();
+        }
+
+        private static void feladat6()
+        {
+
+        }
+
+        private static void feladat5()
+        {
+            int leghosszabb = 0;
+            for (int i = 0; i < Beszelgetes.Beszelgetesek.Count; i++)
+            {
+                if (Beszelgetes.Beszelgetesek[i].Veg - Beszelgetes.Beszelgetesek[i].Kezdet> Beszelgetes.Beszelgetesek[leghosszabb].Veg - Beszelgetes.Beszelgetesek[leghosszabb].Kezdet)
+                {
+                    leghosszabb = i;
+                }
+            }
+            Console.WriteLine($"5. feladat: leghosszabb beszélgetés adatai\n\tKezdeményező:\t{Beszelgetes.Beszelgetesek[leghosszabb].Kezdemenyezo}\n\tFogadó:\t{Beszelgetes.Beszelgetesek[leghosszabb].Fogado}\n\tKezdete:\t{Beszelgetes.Beszelgetesek[leghosszabb].Kezdet}\n\tVége:\t{Beszelgetes.Beszelgetesek[leghosszabb].Veg}");
+        }
+
+        private static void feladat4()
+        {
+            Console.WriteLine($"4.feladat: Tagok száma: {Beszelgetes.Tagok.Count}fő - Beszélgetések: {Beszelgetes.Beszelgetesek.Count}");
+        }
+
+        private static void feladat3()
+        {
+            StreamReader sr = new StreamReader(@"csevegesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                Beszelgetes.Beszelgetesek.Add(new Beszelgetes(sr.ReadLine()));
+            }
+            sr = new StreamReader(@"tagok.txt");
+            while (!sr.EndOfStream)
+            {
+                Beszelgetes.Tagok.Add(sr.ReadLine());
+            }
+            sr.Close();
+        }
+    }
+}
